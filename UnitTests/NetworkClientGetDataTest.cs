cd890e6d403eea23813af0c3e1b2628578fdfcc8
GrandpaNewton <ivan.gromyko.2804@gmail.com>
2024-11-23 16:22:20 +0300

    Adding sending network data unit tests

---
+++ UnitTests/NetworkClientGetDataTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HttpConnectionLibrary;
+using System.Threading;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class NetworkClientGetDataTest
+    {
+        private int _result;
+
+        [TestMethod]
+        public void NetworkDataSendingTestMethod()
+        {
+            var server = new Server();
+            var client = new Client("localhost");
+            var sendingData = 10;
+            client.OnGetData += Client_OnGetData;
+
+            client.GetData<int>();
+            server.UpdateData(sendingData);
+
+            Thread.Sleep(500);
+            Assert.AreEqual(sendingData, _result);
+
+            server.Dispose();
+            client.Dispose();
+        }
+        private void Client_OnGetData(object obj)
+        {
+            _result = (int)obj;
+        }
+    }
+}
---
+++ UnitTests/NetworkUpdateDataTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HttpConnectionLibrary;
+using System.Threading;
+using GameLibrary;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class NetworkUpdateDataTest
+    {
+        private NetworkData _clientResult;
+        private NetworkData _serverResult;
+
+        [TestMethod]
+        public void NetworkDataSendingTestMethod()
+        {
+            var server = new Server();
+            var client = new Client("localhost");
+            client.OnGetData += Client_OnGetData;
+            server.OnGetData += Server_OnGetData;
+            NetworkData clientNetworkData = new NetworkData()
+            {
+                Fuel = 1000
+            };
+            NetworkData serverNetworkData = new NetworkData()
+            {
+                Fuel = 2000
+            };
+
+            client.UpdateData(clientNetworkData);
+            server.UpdateData(serverNetworkData);
+            Thread.Sleep(1000);
+
+            Assert.AreEqual(serverNetworkData.Fuel, _clientResult.Fuel);
+            Assert.AreEqual(clientNetworkData.Fuel, _serverResult.Fuel);
+
+            server.Dispose();
+            client.Dispose();
+        }
+
+        private void Server_OnGetData(object obj)
+        {
+            _serverResult = (NetworkData)obj;
+        }
+
+        private void Client_OnGetData(object obj)
+        {
+            _clientResult = (NetworkData)obj;
+        }
+    }
+}
